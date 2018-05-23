78bca2bd3e879e4bbc840dbe9cc1d3e7034c2f93
Neil Seejoor <nvseejoor@gmail.com>
2018-05-23 19:28:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,9 @@ class ApplicationController < Sinatra::Base
   end
 
   delete '/posts/:id/delete' do
-    
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
   end
 
 end

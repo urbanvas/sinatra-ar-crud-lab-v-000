0c439cfef91eee3385051befc04c5fb8d8f57564
Neil Seejoor <nvseejoor@gmail.com>
2018-05-23 18:01:17 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,15 @@
 class Post < ActiveRecord::Base
   attr_accessor :name, :content
 
+  @@all = []
   def initialize(name, content)
     @name = name
     @content = content
+    @@all << self
   end
 
   def self.all
-
+    @@all
   end
+
 end

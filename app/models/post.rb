956559935d8e3288f13aa0bfa13a209f6266696c
Neil Seejoor <nvseejoor@gmail.com>
2018-05-23 18:02:51 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,4 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
-  attr_accessor :name, :content
-
-  @@all = []
-  # def initialize(name, content)
-  #   @name = name
-  #   @content = content
-  #   @@all << self
-  # end
-
-  def self.all
-    @@all
-  end
-
 end
